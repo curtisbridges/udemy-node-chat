f28e782fcc0b705c0dd6db245e31a4e5bb5e75ef
Curtis Bridges <curtis@curtisbridges.com>
2020-10-13 15:33:38 -0400

    move index.js into src dir
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.listen(port, () => {
     console.log(`Server started http://localhost:${port}`)
