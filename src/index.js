e8965038efa43b8b76bd6d3af998ce2a86553fd6
Curtis Bridges <curtis@curtisbridges.com>
2020-10-13 15:58:41 -0400

    install socket.io, load it in client and server
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+})
+
+server.listen(port, () => {
     console.log(`Server started http://localhost:${port}`)
 })
