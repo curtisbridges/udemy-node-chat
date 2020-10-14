bf437fe550dfe82e0698c82aef133177056e18c0
Curtis Bridges <curtis@curtisbridges.com>
2020-10-14 12:12:41 -0400

    add location share ack
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,25 +1,33 @@
 const socket = io()
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
-    e.preventDefault()
+  e.preventDefault()
 
-    const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
+  const message = e.target.elements.message.value
+  socket.emit('sendMessage', message, (error) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Message delivered.')
+    }
+  })
 })
 
 socket.on('message', (message) => {
-    console.log(message)
+  console.log(message)
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
-    if (!navigator.geolocation) {
-        return alert('Geolocation is not supported by your browser')
-    }
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser')
+  }
 
-    navigator.geolocation.getCurrentPosition((position) => {
-        socket.emit('sendLocation', {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        })
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, () => {
+      console.log('Location shared!')
     })
+  })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -11,20 +12,33 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, '../public')))
 
+
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
+    // send a message to newly connected clients.
     socket.emit('sendMessage', 'Welcome!')
+    // send a message to all clients on new connections.
     socket.broadcast.emit('message', 'A new user has joined.')
 
-    socket.on('sendMessage', (message) => {
-        io.emit('sendMessage', message)
+    // when the server receives a 'sendMessage', send it out to all clients.
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+        io.emit('message', message)
+        callback()
     })
 
-    socket.on('sendLocation', (position) => {
+    // when the server receives a 'sendLocation', send that location to all clients.
+    socket.on('sendLocation', (position, callback) => {
         io.emit('message', `https://google.com/maps?q=${position.latitude},${position.longitude}`)
+        callback()
     })
 
+    // notify all clients that someone disconnected.
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left.')
     })
